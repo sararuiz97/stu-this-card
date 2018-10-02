cfe3384bbbe49fb42bae496d87e907e3f6e35390
David Cabello <david0cabello0@gmail.com>
2018-10-01 19:26:24 -0500

    added navbar and dashboard components

--- stu-this-card-app/src/app/app.module.ts
+++ stu-this-card-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CardComponent
+    CardComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
