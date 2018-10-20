6a805bad7d1967371910741eb1cee0a4b13bd284
Eduardo Merino <eduardo.merino@wizeline.com>
2018-10-20 18:49:41 -0500

    🎨 - add cube

--- stu-this-card-app/client/src/app/components/three-scene/engine.service.ts
+++ stu-this-card-app/client/src/app/components/three-scene/engine.service.ts
@@ -10,25 +10,29 @@ export class EngineService {
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
+  private windowx: number;
+  private windowy: number;
 
   private cube: THREE.Mesh;
 
   createScene(elementId: string): void {
     // The first step is to get the reference of the canvas element from our HTML document
     this.canvas = <HTMLCanvasElement>document.getElementById(elementId);
+    this.windowx = window.innerWidth / 2;
+    this.windowy = window.innerHeight / 2;
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
       alpha: true,    // transparent background
       antialias: true // smooth edges
     });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(this.windowx, this.windowy);
 
     // create the scene
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000
+      75, this.windowx / this.windowy, 0.1, 1000
     );
     this.camera.position.z = 5;
     this.scene.add(this.camera);
@@ -38,11 +42,14 @@ export class EngineService {
     this.light.position.z = 10;
     this.scene.add(this.light);
 
+    this.addCube();
+  }
+
+  addCube() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     this.cube = new THREE.Mesh( geometry, material );
     this.scene.add(this.cube);
-
   }
 
   animate(): void {
@@ -65,9 +72,16 @@ export class EngineService {
     this.renderer.render(this.scene, this.camera);
   }
 
+  recalculateWindow() {
+    this.windowx = window.innerWidth / 2;
+    this.windowy = window.innerHeight / 2;
+  }
+
   resize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+    this.recalculateWindow();
+
+    const width = this.windowx;
+    const height = this.windowy;
 
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
