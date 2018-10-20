be758b8b4c44d15775e379db57e473c537f2b00e
Eduardo Merino <eduardo.merino@wizeline.com>
2018-10-20 18:15:21 -0500

    🚧 - 3d cube

--- stu-this-card-app/client/src/app/components/three-scene/three-scene.component.ts
+++ stu-this-card-app/client/src/app/components/three-scene/three-scene.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EngineService } from './engine.service';
 
 @Component({
   selector: 'app-three-scene',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./three-scene.component.css']
 })
 export class ThreeSceneComponent implements OnInit {
+  private canEleId = 'renderCanvas';
 
-  constructor() { }
+  constructor(private engServ: EngineService) { }
 
   ngOnInit() {
+    this.engServ.createScene(this.canEleId);
+    this.engServ.animate();
   }
-
 }
