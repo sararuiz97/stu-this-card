359c6eb205bc87f62f1f69cfee9e5d4e3a2230f3
Eduardo Merino <eduardo.merino@wizeline.com>
2018-10-21 20:15:37 -0500

    part of merge

--- stu-this-card-app/server/server.js
+++ stu-this-card-app/server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -23,29 +22,3 @@ app.use('/users', require('./controllers/user.controller'));
 
 
 app.listen(4000, () => console.log('Express server running on port 4000'));
-=======
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/cardsDB');
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-   console.log('MongoDB connection succeeded');
-});
-
-app.use(cors());
-app.use(bodyParser.json());
-
-
-app.use('/cards', require('./controllers/card.controller'));
-app.use('/collections', require('./controllers/collection.controller'));
-app.use('/users', require('./controllers/user.controller'));
-
-
-app.listen(4000, () => console.log('Express server running on port 4000'));
->>>>>>> 2eb74104e66b01bb7fb6519bbca6e996cce3d716
