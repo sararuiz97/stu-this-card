0f2e6ca31ca76fc7df0fcc969b6920d86b9637e5
Eduardo Merino <eduardo.merino@wizeline.com>
2018-10-29 19:53:21 -0600

    ✨ feat - generic models

--- stu-this-card-app/client/src/app/components/three-scene/engine.service.ts
+++ stu-this-card-app/client/src/app/components/three-scene/engine.service.ts
@@ -15,11 +15,7 @@ export class EngineService {
   private windowx: number;
   private windowy: number;
 
-  private cube: THREE.Mesh;
-  private sphere: THREE.Mesh;
-  private cone: THREE.Mesh;
-  private human: THREE.Mesh;
-  private atom: THREE.Mesh;
+  private models: THREE.Mesh[] = [];
 
   createScene(elementId: string): void {
     // The first step is to get the reference of the canvas element from our HTML document
@@ -40,68 +36,76 @@ export class EngineService {
     this.camera = new THREE.PerspectiveCamera(
       75, this.windowx / this.windowy, 0.1, 1000
     );
-    // this.camera.position.z = 150;
-    // this.camera.position.y = 90;
-    this.camera.position.z = 5;
     this.scene.add(this.camera);
 
     // soft white light
     this.light = new THREE.AmbientLight( 0x404040 );
-    this.light.position.z = 10;
+    this.light.position.z = 5;
     this.scene.add(this.light);
 
-    // this.addCube();
-    // this.addSphere();
-    // this.addCone();
     // this.addHuman();
-    this.addAtom();
+    // this.addAtom();
+    // this.addShapes();
+    this.camera.position.z = 5;
+    this.addModels('assets/Models/adn.json');
   }
 
   addCube() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     material.wireframe = true;
-    this.cube = new THREE.Mesh( geometry, material );
-    this.scene.add(this.cube);
+    const cube = new THREE.Mesh( geometry, material );
+
+    this.models.push(cube);
   }
 
   addSphere() {
     const geometry = new THREE.SphereGeometry(1, 10, 10);
     const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
     material.wireframe = true;
-    this.sphere = new THREE.Mesh(geometry, material);
-    this.sphere.position.y = 2;
-    this.scene.add(this.sphere);
+    const sphere = new THREE.Mesh(geometry, material);
+    sphere.position.y = 2;
+
+    this.models.push(sphere);
   }
 
   addCone() {
     const geometry = new THREE.ConeGeometry(1, 1, 8);
     const material = new THREE.MeshBasicMaterial( {color: 0xee42f4} );
     material.wireframe = true;
-    this.cone = new THREE.Mesh(geometry, material );
-    this.cone.position.y = -2;
-    this.scene.add(this.cone);
+    const cone = new THREE.Mesh(geometry, material );
+    cone.position.y = -2;
+
+    this.models.push(cone);
   }
 
-  addHuman() {
-    const jsonLoader = new THREE.JSONLoader();
-    jsonLoader.load(
-      'assets/Models/HumanAnatomy.json',
-      (geometry, materials) => {
-        const object = new THREE.Mesh(geometry, materials);
-        this.human = object;
-        this.scene.add(this.human);
-      });
+  addShapes() {
+    this.camera.position.z = 5;
+    this.camera.position.y = 0;
+    this.addCube();
+    this.addCone();
+    this.addSphere();
+
+    this.models.forEach((model) => {
+      this.scene.add(model);
+    });
   }
 
   addAtom() {
+    this.camera.position.z = 5;
+    this.addModels('assets/Models/atom.json');
+  }
+
+  addModels(route: string) {
     const jsonLoader = new THREE.JSONLoader();
     jsonLoader.load(
-      'assets/Models/atom.json',
+      route,
       (geometry, materials) => {
         const object = new THREE.Mesh(geometry, materials);
-        this.atom = object;
-        this.scene.add(this.atom);
+        this.models.push(object);
+        this.models.forEach((model) => {
+          this.scene.add(model);
+        });
       });
   }
 
@@ -120,22 +124,9 @@ export class EngineService {
       this.render();
     });
 
-    // this.cube.rotation.x += 0.01;
-    // this.cube.rotation.y += 0.01;
-
-    // this.cone.rotation.x += 0.01;
-    // this.cone.rotation.y += 0.01;
-
-    // this.sphere.rotation.x += 0.01;
-    // this.sphere.rotation.y += 0.01;
-
-    if (this.human) {
-      this.human.rotation.y += 0.01;
-    }
-
-    if (this.atom) {
-      this.atom.rotation.y += 0.01;
-    }
+    this.models.forEach((model) => {
+      model.rotation.y += 0.01;
+    });
 
     this.renderer.render(this.scene, this.camera);
   }
