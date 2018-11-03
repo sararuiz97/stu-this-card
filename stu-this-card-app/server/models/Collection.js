551e2fd36935c0737ec3ff26c5ff24ecc4793582
Eduardo Merino <eduardo.merino@wizeline.com>
2018-11-03 14:17:43 -0600

    New schema

--- stu-this-card-app/server/models/Collection.js
+++ stu-this-card-app/server/models/Collection.js
@@ -3,22 +3,12 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 let Collection = new Schema({
+  name: String,
+  creator: String,
+  shared: [{
     name: String,
-    creator: String,
-    shared: [{
-      name: String,
-      email: String
-    }],
-    cards: [{
-      front: {
-        text: String,
-        media: String
-      },
-      back: {
-        text: String,
-        media: String
-      }
-    }]
+    email: String
+  }]
 });
 
 export default mongoose.model('Collection', Collection);
